cc6d1660d7c8d4671f93301c0caedd87182de069
unknown <cirejuniorba@gmail.com>
2022-12-19 23:39:19 +0000

    Header section done!

--- src/componets/navbar/Navbar.jsx
+++ src/componets/navbar/Navbar.jsx
@@ -20,31 +20,31 @@ function Navbar() {
   return (
 	<div className='gpt3__navbar'>
 	   <div className="gpt3__navbar-links">
-		<div className="gpt3__navbar-links_logo">
-			<img src={logo} alt="logo" />
-		</div>
-		<div className="gpt3__navbar-links_container">
-			<Menu />
-		</div>
-		<div className="gpt3__navbar-sign">
-			<p>Sign in</p>
-			<button type='button'>Sign up</button>
-		</div>
-		 <div className="gpt3__navbar-menu">
-			{toggleMenu ? 
-			<RiCloseLine  color='#FFF' size={27} onClick={ () => setToggleMenu(false)  }/> : 
-			<RiMenu3Line color='#FFF' size={27} onClick={() => setToggleMenu(true)} />	
-			}
-			{toggleMenu && (
-				<div className="gpt3__navbar-menu_container scale-up-center">
-					<div className="gtp3__navbar-menu_container-links">
-						<Menu />
+			<div className="gpt3__navbar-links_logo">
+				<img src={logo} alt="logo" />
+			</div>
+			<div className="gpt3__navbar-links_container">
+				<Menu />
+			</div>
+			</div>
+			<div className="gpt3__navbar-sign">
+				<p>Sign in</p>
+				<button type='button'>Sign up</button>
+			</div>
+			<div className="gpt3__navbar-menu">
+				{toggleMenu ? 
+				<RiCloseLine  color='#FFF' size={27} onClick={ () => setToggleMenu(false)  }/> : 
+				<RiMenu3Line color='#FFF' size={27} onClick={() => setToggleMenu(true)} />	
+				}
+				{toggleMenu && (
+					<div className="gpt3__navbar-menu_container scale-up-center">
+						<div className="gtp3__navbar-menu_container-links">
+							<Menu />
+						</div>
 					</div>
-				</div>
-			)}
-		 </div>
+				)}
+			</div>
 		</div>
-	</div>
   )
 }
 
